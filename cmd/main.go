8392d41fdffdf235ba773ef599735bbb4eb3de91
théau Yapi <theau.yapi@ynov.com>
2023-09-28 12:33:55 +0200

    commentaire

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,45 @@
+// Le paquet principal pour l'exécutable du jeu "Abdel_Run".
 package main
 
+// Importation des bibliothèques nécessaires.
 import (
 	"image/color"
 	"log"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/LeYapson/Abdel_Run/internal/player"
 )
 
+// Définition du type Game, qui est une struct vide.
+// Elle est utilisée pour implémenter l'interface ebiten.Game.
 type Game struct{}
 
+// Déclaration d'une variable globale "abdel" pour représenter le joueur.
 var abdel *player.Player
 
+// La méthode Update est appelée à chaque frame pour gérer les mises à jour de jeu.
 func (g *Game) Update() error {
+	// Vérifie si la touche de gauche est enfoncée.
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		abdel.MoveLeft()
+		abdel.MoveLeft() // Déplace Abdel vers la gauche.
 	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		abdel.MoveRight()
+		abdel.MoveRight() // Déplace Abdel vers la droite.
 	} else {
+		// Si aucune des touches n'est enfoncée, réinitialise l'état de déplacement d'Abdel.
 		abdel.Moving = false
 	}
 	return nil
 }
 
-
+// La méthode Draw est appelée à chaque frame pour gérer le rendu graphique.
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Remplir l'écran avec une couleur de fond (ici noir)
+	// Définit la couleur de fond de l'écran à noir.
 	screen.Fill(color.Black)
 
-	// Dessiner Abdel en fonction de sa position
+	// Crée une image pour représenter Abdel. Ici, il est représenté par un rectangle blanc.
 	abdelRect := ebiten.NewImage(50, 50)
-	abdelRect.Fill(color.White)  // Pour simplifier, Abdel est un rectangle blanc
+	abdelRect.Fill(color.White)
 
+	// Calcule la position X d'Abdel en fonction de sa position actuelle.
 	var abdelX int
 	switch abdel.Pos {
 	case player.Left:
@@ -41,22 +49,33 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	case player.Right:
 		abdelX = 540
 	}
+
+	// Prépare les options de dessin pour Abdel.
 	opts := &ebiten.DrawImageOptions{}
 	opts.GeoM.Translate(float64(abdelX), 215)
+	
+	// Dessine l'image d'Abdel sur l'écran.
 	screen.DrawImage(abdelRect, opts)
 }
 
+// La méthode Layout définit la taille de la fenêtre.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 1024, 680 // Taille initiale de la fenêtre
+	return 1024, 680
 }
 
+// La fonction main est le point d'entrée du programme.
 func main() {
+	// Initialise Abdel.
 	abdel = player.New()
 
+	// Définit la taille de la fenêtre et son titre.
 	ebiten.SetWindowSize(1024, 600)
 	ebiten.SetWindowTitle("Abdel_Run")
+
+	// Crée une nouvelle instance du jeu et la lance.
 	game := &Game{}
 	if err := ebiten.RunGame(game); err != nil {
+		// Si une erreur se produit lors de l'exécution du jeu, elle est enregistrée et le programme est arrêté.
 		log.Fatal(err)
 	}
 }
