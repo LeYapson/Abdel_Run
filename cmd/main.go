38e7e1c006665d4825cbfca0523a738c74147b2d
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-09-29 09:46:54 +0200

    vgybuhnio

--- cmd/main.go
+++ cmd/main.go
@@ -1,84 +1,108 @@
-// Le paquet principal pour l'exécutable du jeu "Abdel_Run".
 package main
 
-// Importation des bibliothèques nécessaires.
 import (
+	"image"
 	"image/color"
+	"image/draw"
+	"image/gif"
 	"log"
+	"os"
 
 	"github.com/LeYapson/Abdel_Run/internal/player"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-// Définition du type Game, qui est une struct vide.
-// Elle est utilisée pour implémenter l'interface ebiten.Game.
-type Game struct{}
+type Game struct {
+	gifImages    []*ebiten.Image
+	currentFrame int
+	counter      int
+	delays       []int
+}
 
-// Déclaration d'une variable globale "abdel" pour représenter le joueur.
 var abdel *player.Player
 
-// La méthode Update est appelée à chaque frame pour gérer les mises à jour de jeu.
 func (g *Game) Update() error {
-	// Vérifie si la touche de gauche est enfoncée.
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		abdel.MoveLeft() // Déplace Abdel vers la gauche.
+		abdel.MoveLeft()
 	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		abdel.MoveRight() // Déplace Abdel vers la droite.
+		abdel.MoveRight()
 	} else {
-		// Si aucune des touches n'est enfoncée, réinitialise l'état de déplacement d'Abdel.
 		abdel.Moving = false
+	}
+
+	// Update GIF animation
+	g.counter++
+	if g.counter > g.delays[g.currentFrame] {
+		g.currentFrame = (g.currentFrame + 1) % len(g.gifImages)
+		g.counter = 0
 	}
 	return nil
 }
 
-// La méthode Draw est appelée à chaque frame pour gérer le rendu graphique.
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Définit la couleur de fond de l'écran à noir.
-	screen.Fill(color.RGBA{0x80, 0xa0, 0xc0, 0xff})
-	ebitenutil.DebugPrintAt(screen, "Hello World", 0, 150)
+	screen.Fill(color.Black)
 
-	// Crée une image pour représenter Abdel. Ici, il est représenté par un rectangle blanc.
-	abdelRect := ebiten.NewImage(50, 50)
-	abdelRect.Fill(color.White)
-
-	// Calcule la position X d'Abdel en fonction de sa position actuelle.
 	var abdelX int
 	switch abdel.Pos {
 	case player.Left:
 		abdelX = 50
 	case player.Center:
-		abdelX = 225
+		abdelX = 295
 	case player.Right:
-		abdelX = 400
+		abdelX = 540
 	}
 
-	// Prépare les options de dessin pour Abdel.
 	opts := &ebiten.DrawImageOptions{}
-	opts.GeoM.Translate(float64(abdelX), 500)
-
-	// Dessine l'image d'Abdel sur l'écran.
-	screen.DrawImage(abdelRect, opts)
+	opts.GeoM.Translate(float64(abdelX), 215)
+	screen.DrawImage(g.gifImages[g.currentFrame], opts)
 }
 
-// La méthode Layout définit la taille de la fenêtre.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 500, 600
+	return 1024, 680
 }
 
-// La fonction main est le point d'entrée du programme.
+func loadGIF(path string) ([]*ebiten.Image, []int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer file.Close()
+
+	g, err := gif.DecodeAll(file)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	images := make([]*ebiten.Image, len(g.Image))
+	delays := make([]int, len(g.Image))
+	previous := image.NewRGBA(g.Image[0].Bounds())
+
+	for i, img := range g.Image {
+		draw.Draw(previous, img.Bounds(), img, image.Point{}, draw.Over)
+		rgba := image.NewRGBA(g.Image[0].Bounds())
+		draw.Draw(rgba, g.Image[0].Bounds(), previous, image.Point{}, draw.Src)
+		images[i] = ebiten.NewImageFromImage(rgba)
+		delays[i] = g.Delay[i]
+	}
+	return images, delays, nil
+}
+
 func main() {
-	// Initialise Abdel.
 	abdel = player.New()
 
-	// Définit la taille de la fenêtre et son titre.
+	// Load the GIF for Abdel
+	gifImages, delays, err := loadGIF("../assets/images/abdel_run.gif")
+	if err != nil {
+		log.Fatalf("Error loading Abdel's GIF: %v", err)
+	}
+
 	ebiten.SetWindowSize(1024, 600)
 	ebiten.SetWindowTitle("Abdel_Run")
-
-	// Crée une nouvelle instance du jeu et la lance.
-	game := &Game{}
+	game := &Game{
+		gifImages: gifImages,
+		delays:    delays,
+	}
 	if err := ebiten.RunGame(game); err != nil {
-		// Si une erreur se produit lors de l'exécution du jeu, elle est enregistrée et le programme est arrêté.
 		log.Fatal(err)
 	}
 }
