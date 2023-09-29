5cf70a5701d7643fe45ffad23e2731391fffac80
LeYapson <theau.yapi@ynov.com>
2023-09-29 15:00:45 +0200

    dgsgs

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strconv"
-
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
@@ -14,7 +12,6 @@ const (
 func main() {
 	rl.InitWindow(screenWidth, screenHeight, "ABDEL RUN!!!")
 	rl.SetTargetFPS(60)
-
 	rl.InitAudioDevice() // Initialise le module audio
 
 	frameCounter := 0
@@ -32,7 +29,13 @@ func main() {
 	gravity := float32(1.0)
 	jumpStrength := float32(-20.0)
 
+	// Charger l'image de fond et la musique une seule fois
+	bgImage := rl.LoadTexture("../assets/images/TitleScreen.jpg")
+	bgMusic := rl.LoadMusicStream("../assets/sounds/AbdelRunSong.ogg")
+
 	for !rl.WindowShouldClose() {
+		rl.UpdateMusicStream(bgMusic)
+
 		switch currentScreen {
 		case 0:
 			rl.BeginDrawing()
@@ -41,16 +44,13 @@ func main() {
 			frameCounter++
 			if frameCounter > 120 {
 				currentScreen++
+				rl.PlayMusicStream(bgMusic)
 			}
-		case 1:
-			// Charger l'image de fond
-			bgImage := rl.LoadTexture("../assets/images/TitleScreen.jpg")
-			// Charger la musique de fond
-			bgMusic := rl.LoadMusicStream("../assets/sounds/AbdelRunSong.ogg")
-			rl.PlayMusicStream(bgMusic)
 
-			str := strconv.Itoa(currentScreen)
-			rl.DrawText(str, 10, 0, 20, rl.Gray)
+		case 1:
+			rl.BeginDrawing()
+			rl.ClearBackground(rl.White)
+			rl.DrawTexture(bgImage, 0, 0, rl.White)
 
 			buttons := []struct {
 				Bounds rl.Rectangle
@@ -61,52 +61,27 @@ func main() {
 				{rl.NewRectangle(screenWidth-325, screenHeight/2+60, 150, 40), "Quit"},
 			}
 
-			for !rl.WindowShouldClose() {
-				rl.UpdateMusicStream(bgMusic) // Mettre à jour le flux de la musique
-
-				rl.BeginDrawing()
-				rl.ClearBackground(rl.White)
-				// Dessiner l'image de fond
-				rl.DrawTexture(bgImage, 0, 0, rl.White)
-
-				for _, button := range buttons {
-					color := rl.Yellow
-					if rl.CheckCollisionPointRec(rl.GetMousePosition(), button.Bounds) {
-						color = rl.DarkGray
-						if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-							switch button.Text {
-							case "Quit":
-
-								rl.UnloadMusicStream(bgMusic) // Libérer la musique de la mémoire
-								rl.CloseAudioDevice()         // Fermer le périphérique audio
-								rl.CloseWindow()
-								return
-							case "Play":
-								//rl.EndDrawing()
-								//rl.UnloadMusicStream(bgMusic) // Libérer la musique de la mémoire
-								//rl.CloseAudioDevice()
-								currentScreen = 2
-								rl.DrawText(str, 10, 0, 20, rl.Gray)
-							}
-
-							//if button.Text == "Quit" {
-
-							//} else if button.Text == "Play" {
-							//rl.EndDrawing()
-							//rl.UnloadMusicStream(bgMusic) // Libérer la musique de la mémoire
-							//rl.CloseAudioDevice()
-
-							//rl.CloseWindow()
-
-							//}
-							// Gérer les autres boutons ici
+			for _, button := range buttons {
+				color := rl.Yellow
+				if rl.CheckCollisionPointRec(rl.GetMousePosition(), button.Bounds) {
+					color = rl.DarkGray
+					if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+						switch button.Text {
+						case "Quit":
+							rl.UnloadMusicStream(bgMusic)
+							rl.CloseAudioDevice()
+							rl.CloseWindow()
+							return
+						case "Play":
+							currentScreen = 2
 						}
 					}
-					rl.DrawRectangleRec(button.Bounds, color)
-					rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
 				}
-				rl.EndDrawing()
+				rl.DrawRectangleRec(button.Bounds, color)
+				rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
 			}
+			rl.EndDrawing()
+
 		case 2:
 			if rl.IsKeyPressed(rl.KeySpace) && !isJumping {
 				isJumping = true
@@ -121,13 +96,17 @@ func main() {
 					isJumping = false
 				}
 			}
+
 			rl.BeginDrawing()
 			rl.ClearBackground(rl.White)
-			rl.DrawText("PRESS SPACE to PAUSE MOVEMENT", 10, 0, 20, rl.Gray)
+			rl.DrawText("PRESS SPACE to JUMP", 10, 0, 20, rl.Gray)
 			rl.DrawRectangleRec(player, rl.Red)
-			//rl.DrawTexture(texture, screenWidth/2-texture.width/2, screenHeight/2-texture.height/2, WHITE)
 			rl.EndDrawing()
 		}
-
 	}
+
+	rl.UnloadTexture(bgImage)
+	rl.UnloadMusicStream(bgMusic)
+	rl.CloseAudioDevice()
+	rl.CloseWindow()
 }
