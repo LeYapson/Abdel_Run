7018d99ad3953c749809d2916f237a61c6173bcc
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 15:34:21 +0200

    version qui marche

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 
 	ui "github.com/LeYapson/Abdel_Run/internal/ui"
@@ -68,7 +69,69 @@ func main() {
 			currentScreen = ui.TitleScreen(currentScreen, bgImage, bgMusic)
 
 		case 2:
-			currentScreen = lgs.Gameplay(currentScreen, screenWidth, screenHeight, fps)
+			//Saut du personnage
+			if rl.IsKeyPressed(int32(toucheSaut)) && !isJumping {
+				isJumping = true
+				velocity = jumpStrength
+				//fmt.Println(velocity)
+			}
+
+			//Retombée du personnage
+			if isJumping {
+				//fmt.Println(velocity)
+				//frate := float32(velocity * (float32(fps) * rl.GetFrameTime()))
+				player1.Y += velocity
+				velocity += gravity
+				if player1.Y > screenHeight-player1.Height {
+					player1.Y = screenHeight - player1.Height
+					isJumping = false
+				}
+			}
+			stringToucheSaut := strconv.FormatInt(int64(toucheSaut), 10)
+			texteToucheSaut := "Press" + stringToucheSaut + " to jump"
+
+			fmt.Println(rl.GetFrameTime())
+
+			rl.BeginDrawing()
+			rl.ClearBackground(rl.White)
+			rl.DrawText(texteToucheSaut, 10, 0, 20, rl.Gray)
+			rl.DrawRectangleRec(player1, rl.Red)
+			rl.DrawRectangleRec(player2, rl.Blue)
+
+			collision := rl.CheckCollisionRecs(player1, player2)
+
+			if collision {
+				rl.DrawText("COLLISION!", 600, 250, 20, rl.Red)
+			}
+
+			//Création du bouton back
+			buttons := []struct {
+				Bounds rl.Rectangle
+				Text   string
+			}{
+				{rl.NewRectangle(screenWidth/20, screenHeight/20, 150, 40), "Back"},
+			}
+
+			//Fonctionnalité du bouton
+			for _, button := range buttons {
+				color := rl.Yellow
+				if rl.CheckCollisionPointRec(rl.GetMousePosition(), button.Bounds) {
+					color = rl.DarkGray
+					if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+						switch button.Text {
+						case "Back":
+							currentScreen = 1
+						}
+					}
+				}
+				//Affichage du bouton
+				rl.DrawRectangleRounded(button.Bounds, ratioArrondiRec, segmentRec, color)
+				rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
+			}
+
+			rl.EndDrawing()
+
+			//currentScreen = lgs.Gameplay(currentScreen, screenWidth, screenHeight, fps)
 		case 3:
 			rl.BeginDrawing()
 			rl.DrawTexture(bgSettings, 0, 0, rl.White)
