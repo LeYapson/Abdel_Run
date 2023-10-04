a4d9373d6489d29e505f0c9c46c9a252e5413d62
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-04 14:51:11 +0200

    fichier titleScreen + saut fonctionne

--- cmd/main.go
+++ cmd/main.go
@@ -41,8 +41,10 @@ func main() {
 	velocity := float32(0.0)
 	gravity := float32(1.0)
 	jumpStrength := float32(-20.0)
+
 	ratioArrondiRec := float32(0.5)
 	segmentRec := int32(0)
+
 	toucheSaut := rl.KeySpace //Initialise la touche de saut
 	//animFrames := int32(0)
 	//p := &animFrames
@@ -59,49 +61,10 @@ func main() {
 
 		switch currentScreen {
 		case 0:
-
 			currentScreen, frameCounter = ui.LogoScreen(currentScreen, frameCounter, bgLogo, bgMusic)
-
 		case 1:
-			rl.BeginDrawing()
-			rl.DrawTexture(bgImage, 0, 0, rl.White)
 
-			if !rl.IsMusicStreamPlaying(bgMusic) {
-				rl.UpdateMusicStream(bgMusic)
-				rl.PlayMusicStream(bgMusic)
-			}
-			buttons := []struct {
-				Bounds rl.Rectangle
-				Text   string
-			}{
-				{rl.NewRectangle(screenWidth-325, screenHeight/2-40, 150, 40), "Play"},
-				{rl.NewRectangle(screenWidth-325, screenHeight/2+10, 150, 40), "Settings"},
-				{rl.NewRectangle(screenWidth-325, screenHeight/2+60, 150, 40), "Quit"},
-			}
-
-			for _, button := range buttons {
-				color := rl.Yellow
-				if rl.CheckCollisionPointRec(rl.GetMousePosition(), button.Bounds) {
-					color = rl.DarkGray
-					if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-						switch button.Text {
-						case "Quit":
-							rl.UnloadMusicStream(bgMusic)
-							rl.CloseAudioDevice()
-							rl.CloseWindow()
-							return
-						case "Play":
-							rl.StopMusicStream(bgMusic)
-							currentScreen = 2
-						case "Settings":
-							currentScreen = 3
-						}
-					}
-				}
-				rl.DrawRectangleRounded(button.Bounds, ratioArrondiRec, segmentRec, color)
-				rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
-			}
-			rl.EndDrawing()
+			currentScreen = ui.TitleScreen(currentScreen, bgImage, bgMusic)
 
 		case 2:
 			//Saut du personnage
--- internal/ui/titleScreen.go
+++ internal/ui/titleScreen.go
@@ -8,20 +8,17 @@ const (
 	screenWidth  = 1024
 	screenHeight = 600
 )
- 
-func TitleScreen() {
-	var currentScreen = 1
-	rl.InitWindow(screenWidth, screenHeight, "ABDEL RUN!!!")
-	rl.SetTargetFPS(60)
 
-	rl.InitAudioDevice() // Initialise le module audio
+func TitleScreen(currentScreen int, bgImage rl.Texture2D, bgMusic rl.Music) int {
+	ratioArrondiRec := float32(0.5)
+	segmentRec := int32(0)
+	rl.BeginDrawing()
+	rl.DrawTexture(bgImage, 0, 0, rl.White)
 
-	// Charger l'image de fond
-	bgImage := rl.LoadTexture("../assets/images/TitleScreen.jpg")
-	// Charger la musique de fond
-	bgMusic := rl.LoadMusicStream("../assets/sounds/AbdelRunSong.ogg")
-	rl.PlayMusicStream(bgMusic)
-
+	if !rl.IsMusicStreamPlaying(bgMusic) {
+		rl.UpdateMusicStream(bgMusic)
+		rl.PlayMusicStream(bgMusic)
+	}
 	buttons := []struct {
 		Bounds rl.Rectangle
 		Text   string
@@ -31,38 +28,28 @@ func TitleScreen() {
 		{rl.NewRectangle(screenWidth-325, screenHeight/2+60, 150, 40), "Quit"},
 	}
 
-	for !rl.WindowShouldClose() {
-		rl.UpdateMusicStream(bgMusic) // Mettre à jour le flux de la musique
-
-		rl.BeginDrawing()
-		rl.ClearBackground(rl.White)
-		// Dessiner l'image de fond
-		rl.DrawTexture(bgImage, 0, 0, rl.White)
-
-		for _, button := range buttons {
-			color := rl.Yellow
-			if rl.CheckCollisionPointRec(rl.GetMousePosition(), button.Bounds) {
-				color = rl.DarkGray
-				if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
-					switch button.Text {
-					case "Quit":
-						rl.UnloadMusicStream(bgMusic)
-						rl.CloseAudioDevice()
-						rl.CloseWindow()
-						return
-					case "Play":
-						rl.UnloadMusicStream(bgMusic)
-						rl.CloseAudioDevice()
-						currentScreen += 1
-					case "Settings":
-						currentScreen += 2
-					}
-
+	for _, button := range buttons {
+		color := rl.Yellow
+		if rl.CheckCollisionPointRec(rl.GetMousePosition(), button.Bounds) {
+			color = rl.DarkGray
+			if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
+				switch button.Text {
+				case "Play":
+					rl.StopMusicStream(bgMusic)
+					currentScreen = 2
+				case "Settings":
+					currentScreen = 3
+				case "Quit":
+					rl.UnloadMusicStream(bgMusic)
+					rl.CloseAudioDevice()
+					rl.CloseWindow()
+					//return
 				}
 			}
-			rl.DrawRectangleRec(button.Bounds, color)
-			rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
 		}
-		rl.EndDrawing()
-}
+		rl.DrawRectangleRounded(button.Bounds, ratioArrondiRec, segmentRec, color)
+		rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
+	}
+	rl.EndDrawing()
+	return currentScreen
 }
