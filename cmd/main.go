e96eec857e1c5574c067111825ceeca42e2793df
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-10-06 09:04:33 +0200

    com

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,20 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
+// Initialise les constantes de la taille de la fenêtre
 const (
 	screenWidth  = 1024
 	screenHeight = 600
 )
 
+// Structure player avec sa hitbox et ses points de vie
 type player struct {
 	hitbox rl.Rectangle
 	health int
+}
+
+type platform struct {
+	hitbox rl.Rectangle
 }
 
 func newPlayer() *player {
@@ -24,38 +30,39 @@ func newPlayer() *player {
 	return &p
 }
 
-var fps = 60
-var toucheSaut = int32(rl.KeySpace) //Initialise la touche de saut
+func newPlatform(hitbox rl.Rectangle) *platform {
+	p := platform{hitbox}
+	return &p
+}
+
+func collision(rec1 rl.Rectangle, rec2 rl.Rectangle) bool {
+	return rl.CheckCollisionRecs(rec1, rec2)
+}
+
+var (
+	fps        = 60
+	toucheSaut = int32(rl.KeySpace) //Initialise la touche de saut
+
+	frameCounter  = 0
+	currentScreen = 0
+
+	player1 = newPlayer()
+
+	platform1 = newPlatform(rl.NewRectangle(screenWidth/8, screenHeight-150, 150, 50))
+
+	isJumping    = false
+	velocity     = float32(0.0)
+	gravity      = float32(1.0)
+	jumpStrength = float32(-20.0)
+
+	ratioArrondiRec = float32(0.5)
+	segmentRec      = int32(0)
+)
 
 func main() {
 	rl.InitWindow(screenWidth, screenHeight, "ABDEL RUN!!!")
-	rl.SetTargetFPS(int32(fps))
-	rl.InitAudioDevice() // Initialise le module audio
-
-	frameCounter := 0
-	currentScreen := 0
-
-	player := newPlayer()
-
-	//personne := models.NewPerson()
-
-	player2 := rl.Rectangle{
-		X:      screenWidth / 8,
-		Y:      screenHeight - 150,
-		Width:  50,
-		Height: 50,
-	}
-
-	isJumping := false
-	velocity := float32(0.0)
-	gravity := float32(1.0)
-	jumpStrength := float32(-20.0)
-
-	ratioArrondiRec := float32(0.5)
-	segmentRec := int32(0)
-
-	//animFrames := int32(0)
-	//p := &animFrames
+	rl.SetTargetFPS(int32(fps)) //Initialise le nombre d'images par seconde
+	rl.InitAudioDevice()        // Initialise le module audio
 
 	// Charger l'image de fond et la musique une seule fois
 	bgLogo := rl.LoadTexture("../assets/images/logo.jpg")
@@ -63,7 +70,6 @@ func main() {
 	bgMusic := rl.LoadMusicStream("../assets/sounds/AbdelRunSong.ogg")
 	playMusic := rl.LoadMusicStream("../assets/sounds/playSong.ogg")
 	bgSettings := rl.LoadTexture("../assets/images/Settings.png")
-	//gifAbdel := rl.LoadImageAnim("../assets/abdel_run.gif", p)
 
 	for !rl.WindowShouldClose() {
 		rl.UpdateMusicStream(bgMusic)
@@ -73,9 +79,7 @@ func main() {
 		case 0:
 			currentScreen, frameCounter = ui.LogoScreen(currentScreen, frameCounter, bgLogo, bgMusic)
 		case 1:
-
 			currentScreen = ui.TitleScreen(currentScreen, bgImage, bgMusic)
-
 		case 2:
 			if !rl.IsMusicStreamPlaying(playMusic) {
 				rl.UpdateMusicStream(playMusic)
@@ -85,9 +89,9 @@ func main() {
 			stringToucheSaut := strconv.FormatInt(int64(toucheSaut), 10)
 			texteToucheSaut := "Press" + stringToucheSaut + " to jump"
 
-			collision := rl.CheckCollisionRecs(player.hitbox, player2)
+			//collision := rl.CheckCollisionRecs(player1.hitbox, platform1.hitbox)
 
-			if collision {
+			if collision(player1.hitbox, platform1.hitbox) {
 				rl.DrawText("COLLISION!", 600, 250, 20, rl.Red)
 			}
 			//Saut du personnage
@@ -99,43 +103,43 @@ func main() {
 			//Retombée du personnage
 			if isJumping {
 				//frate := float32(velocity * (float32(fps) * rl.GetFrameTime()))
-				if collision && player.hitbox.Y-player.hitbox.Y < player2.Y-player.hitbox.Height {
-					velocity = player2.Height / 2
-					player.hitbox.Y += velocity
+				if collision(player1.hitbox, platform1.hitbox) && player1.hitbox.Y-player1.hitbox.Y < platform1.hitbox.Y-player1.hitbox.Height {
+					velocity = platform1.hitbox.Height / 2
+					player1.hitbox.Y += velocity
 					velocity = 1.0
 				}
 				// if player.hitbox.Y >= player2.Y-player2.Height && player.hitbox.X+50 > player2.X && !collision {
 				// 	velocity = 1.0
 				// }
-				player.hitbox.Y += velocity
+				player1.hitbox.Y += velocity
 				velocity += gravity
-				fmt.Println(player.hitbox.Y)
+				fmt.Println(player1.hitbox.Y)
 
-				if player.hitbox.Y > screenHeight-player.hitbox.Height {
-					player.hitbox.Y = screenHeight - player.hitbox.Height
+				if player1.hitbox.Y > screenHeight-player1.hitbox.Height {
+					player1.hitbox.Y = screenHeight - player1.hitbox.Height
 					isJumping = false
 				}
 			}
 
-			if player.hitbox.Y <= player2.Y && collision {
-				player.hitbox.Y = player2.Y - player2.Height
+			if player1.hitbox.Y <= platform1.hitbox.Y && collision(player1.hitbox, platform1.hitbox) {
+				player1.hitbox.Y = platform1.hitbox.Y - platform1.hitbox.Height
 				isJumping = false
 			}
 
-			deplacementPlayer2 := float32(2.5)
-			if collision {
-				deplacementPlayer2 = 0
+			deplacementPlatform := float32(2.5)
+			if collision(player1.hitbox, platform1.hitbox) {
+				deplacementPlatform = 0
 			}
-			player2.X += deplacementPlayer2
-			if player2.X >= float32(rl.GetScreenWidth()-700) {
-				player2.X = 0
+			platform1.hitbox.X += deplacementPlatform
+			if platform1.hitbox.X >= float32(rl.GetScreenWidth()-700) {
+				platform1.hitbox.X = 0
 			}
 
 			rl.BeginDrawing()
 			rl.ClearBackground(rl.White)
 			rl.DrawText(texteToucheSaut, 10, 0, 20, rl.Gray)
-			rl.DrawRectangleRec(player.hitbox, rl.Red)
-			rl.DrawRectangleRec(player2, rl.Blue)
+			rl.DrawRectangleRec(player1.hitbox, rl.Red)
+			rl.DrawRectangleRec(platform1.hitbox, rl.Blue)
 
 			//Création du bouton back
 			buttons := []struct {
@@ -153,6 +157,7 @@ func main() {
 					if rl.IsMouseButtonPressed(rl.MouseLeftButton) {
 						switch button.Text {
 						case "Back":
+							rl.StopMusicStream(playMusic)
 							currentScreen = 1
 						}
 					}
@@ -161,10 +166,7 @@ func main() {
 				rl.DrawRectangleRounded(button.Bounds, ratioArrondiRec, segmentRec, color)
 				rl.DrawText(button.Text, int32(button.Bounds.X+button.Bounds.Width/2)-rl.MeasureText(button.Text, 20)/2, int32(button.Bounds.Y+10), 20, rl.Black)
 			}
-
 			rl.EndDrawing()
-
-			//currentScreen = lgs.Gameplay(currentScreen, screenWidth, screenHeight, fps)
 		case 3:
 			rl.BeginDrawing()
 			rl.DrawTexture(bgSettings, 0, 0, rl.White)
@@ -207,7 +209,6 @@ func main() {
 							return
 						case "Change jump key":
 							currentScreen = 5
-
 						}
 					}
 				}
@@ -233,7 +234,7 @@ func main() {
 			rl.BeginDrawing()
 			rl.ClearBackground(rl.Green)
 
-			//Boutons back et quit dans settings
+			//Bouton back
 			buttons := []struct {
 				Bounds rl.Rectangle
 				Text   string
@@ -250,7 +251,6 @@ func main() {
 						switch button.Text {
 						case "Back":
 							currentScreen = 3
-
 						}
 					}
 				}
